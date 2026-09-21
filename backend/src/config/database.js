8bedf567eb53dba1b0191bdbb88eaa08eca41597
chauphamsongthan-kim <Chau.phamsongthan@hcmut.edu.vn>
2026-09-21 20:47:27 +0700

    Fix MySQL UTF-8 encoding

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -15,6 +15,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   dateStrings: true, // trả DATE dạng 'YYYY-MM-DD' thay vì object Date
+  charset: 'utf8mb4',
 });
 
 // Kiểm tra kết nối khi khởi động server
